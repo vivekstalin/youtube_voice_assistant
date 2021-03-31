e46984f75735fd47260ea80f4b2c6064667bf573
vivek stalin <vivek@acalvio.com>
2021-03-31 21:41:43 +0530

    latest update
    custom search func

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Doesn't need much except Python and few Python Packages
   1. SpeechRecognition
   2. pyttsx3
   3. playsound
-  4. pywhatkit
+  4. Basic packages like requests, webbrowser which should available by default
   ```
 ### Installation
 1. Clone the repo
@@ -29,11 +29,10 @@ The sample version I have put here works in the below way:
 ```
 1. Run the main file ~/src/voice.py
 2. It would start taking to you. My Bot name is Jarvis
-3. Say after beep <bot name> <play> <content in youtube>
+3. Ask you to talk after beep, So once you hear the beep wait for one or two seconds and then talk 
+   Voice Input Format: <bot name> <play> <content in youtube>
    Ex1: Jarvis play faded song
    Ex2: Jarvis play Falcon heavy test flight
-4. There you go, you will see the youtube video getting played automatically on youtube in the browser
-PS:
-There would be debris file created by pywhatkit package when you run it for first time, dont mind just ignore.
+4. There you go, it will take few seconds for the search to happen in the background and then you will see the youtube video getting played automatically on youtube in the browser
 ```
 
--- src/voice.py
+++ src/voice.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 import pyttsx3
-import pywhatkit
+import requests
+import webbrowser as web
 from playsound import playsound
 
 def jarvis_talk(text=''):
@@ -16,7 +17,7 @@ def jarvis_talk(text=''):
             jarvis.say(text)
         jarvis.runAndWait()
     except Exception as e:
-        print("Exception "+e)
+        print("Exception in jarvis_talk() "+e)
 
 def jarvis_listen():
     try:
@@ -24,7 +25,7 @@ def jarvis_listen():
         with sr.Microphone() as talkie:
             playsound('beep_1.mp3')
             print("Listening....")
-            voice = listener.listen(talkie,timeout=7)
+            voice = listener.listen(talkie,timeout=5)
             command = listener.recognize_google(voice).lower()
             print(command)
             if 'jarvis' in command and 'play' in command:
@@ -34,15 +35,34 @@ def jarvis_listen():
                 jarvis_talk("Sorry I Did not hear you clearly")
                 jarvis_talk("Can you please say that again")
     except Exception as e:
-        print("Exception "+e)
+        print("Exception in jarvis_listen() "+e)
 
 def jarvis_play(song):
     try:
         song = song.lower().replace('play','').strip()
         jarvis_talk('Playing '+song)
-        pywhatkit.playonyt(song)
+        play_yt(song)
     except Exception as e:
-        print("Exception "+e)
+        print("Exception in jarvis_play() "+e)
+
+def play_yt(topic):
+    try:
+        url = 'https://www.youtube.com/results?q=' + topic
+        count = 0
+        cont = requests.get(url)
+        data = cont.content
+        data = str(data)
+        lst = data.split('"')
+        for i in lst:
+            count += 1
+            if i == 'WEB_PAGE_TYPE_WATCH':
+                break
+        if lst[count - 5] == "/results":
+            raise Exception("No video found.")
+        web.open("https://www.youtube.com" + lst[count - 5])
+        return "https://www.youtube.com" + lst[count - 5]
+    except Exception as e:
+        print("Exception in play_yt() "+e)
 
 if __name__ == '__main__':
         jarvis_talk()
